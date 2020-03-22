5c66463a1ea3bc9d3b009b79751c8448cfba2cda
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-03-22 18:58:11 +0100

    sezione 3 parte 4

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ import Header from "./components/Header";
 import WeatherCard from './components/WeatherCard';
 
 export default class App extends React.Component {
+  state = {
+    cities: ['Roma', 'Milano', 'Parigi'],
+  }
   render() {
+    const cities = this.state.cities.map((city, index) => (
+      <WeatherCard title={city} />
+    ))
     return (
       <View style={styles.container}>
         <Header title={"Meteo App"} />
         <ScrollView contentContainerStyle={styles.cardContainer}>
-          <WeatherCard title={'Roma'} />
-          <WeatherCard title={'Roma'} />
-          <WeatherCard title={'Roma'} />
-          <WeatherCard title={'Roma'} />
+          {cities}
         </ScrollView>
       </View>
     );
