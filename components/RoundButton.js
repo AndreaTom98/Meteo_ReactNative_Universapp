e6fdcbdab085b559cdb69018332fc7d95439ecbd
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-03-23 19:22:28 +0100

    fix

--- components/RoundButton.js
+++ components/RoundButton.js
@@ -3,6 +3,7 @@ import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
 
 const RoundButton = (props) => {
     const plusImage = require('../assets/plus.png');
+    const isPlus = props.plusButton;
     return (
         <TouchableOpacity onPress={props.onPress} style={styles.container}>
             <Image source={plusImage} style={styles.plusImage} />
