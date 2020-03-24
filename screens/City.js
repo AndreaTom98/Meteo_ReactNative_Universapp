604bd959e98905113352f93362a389a7390b4905
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-03-24 20:58:48 +0100

    fix 11

--- screens/City.js
+++ screens/City.js
@@ -3,7 +3,7 @@ import {View, Text, Button} from 'react-native';
 
 
 const City = (props) => {
-    const navigation = props.navigation
+    const navigation = props.navigation;
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>hello from city</Text>
