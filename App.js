63acd039712af983f6f0a6d5de456eff45635601
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-03-24 20:25:51 +0100

    sezione 3 parte 10

--- App.js
+++ App.js
@@ -1,56 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
-import Header from "./components/Header";
-import WeatherCard from './components/WeatherCard';
-import AddCityModal from './components/AddCityModal';
-import RoundButton from './components/RoundButton';
+import {} from "react-native";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from './screens/Home';
+import City from './screens/City';
 
-export default class App extends React.Component {
-  state = {
-    cities: [],
-    visible: false,
-  }
-  addCity = (city) => {
-    this.setState(prevState => {
-      return {
-        cities: prevState.cities.concat(city)
-      }
-    }, this.closeModal())
-  }
-  openModal = () => {
-    this.setState({
-      visible: true,
-    })
-  }
-  closeModal = () => {
-    this.setState({
-      visible: false,
-    })
-  }
-  render() {
-    const cities = this.state.cities.map((city, index) => (
-      <WeatherCard key={index} title={city} />
-    ))
-    return (
-      <View style={styles.container}>
-        <AddCityModal addCity={this.addCity} visible={this.state.visible} closeModal={this.closeModal} />
-        <Header title={"Meteo App"} />
-        <ScrollView contentContainerStyle={styles.cardContainer}>
-          {cities}
-          <RoundButton plusButton={true} onPress={this.openModal} />
-        </ScrollView>
-      </View>
-    );
-  }
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  cardContainer: {
-    alignItems: 'center',
-    flex: 1,
-    marginTop: 20
-  }
-});
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="City" component={City} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ screens/City.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const City = () => {
+    return (
+        <View>
+            <Text>hello from city</Text>
+        </View>
+    )
+}
+
+export default City;
