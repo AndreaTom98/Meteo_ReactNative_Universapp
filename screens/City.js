7cdc265003d51262f56e9d505ec23c262e6f500d
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-04-06 20:47:02 +0200

    sezione 5 parte 7

--- screens/City.js
+++ screens/City.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 
 
 const City = ({route}) => {
@@ -7,9 +7,41 @@ const City = ({route}) => {
     console.log('from city.js:', data)
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>hello from {data.city.name}</Text>
+            {/* frontContainer */}
+            <View style={styles.frontContainer}>
+                <Text>Milano</Text>
+                <Text>soleggiato</Text>
+                <Text>21 gradi</Text>
+            </View>
+            <ScrollView horizontal={true} style={styles.scrollContainer}>
+                <Text>dfrgthyjukiougbf</Text>
+                <Text>dfrgthyjukiougbf</Text>
+                <Text>dfrgthyjukiougbf</Text>
+                <Text>dfrgthyjukiougbf</Text>
+                <Text>dfrgthyjukiougbf</Text>
+                <Text>dfrgthyjukiougbf</Text>
+                <Text>dfrgthyjukiougbf</Text>
+            </ScrollView>
+            <View style={styles.daysContainer}></View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    frontContainer: {
+        flex: 2,
+        width: '100%',
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    scrollContainer: {
+        width: '100%',
+        flex:1
+    },
+    daysContainer: {
+        width: '100%',
+        flex: 2
+    }
+})
 
 export default City;
